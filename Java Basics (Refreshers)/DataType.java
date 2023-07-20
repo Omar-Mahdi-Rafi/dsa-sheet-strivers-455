de6af638fc31e0eb04c979773edf3b393b479a34
OMAR MAHDI RAFI <97361722+Omar-Mahdi-Rafi@users.noreply.github.com>
2023-07-20 21:12:18 +0600

    Create DataType.java

---
+++ Java Basics (Refreshers)/DataType.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public static int dataTypes(String type) {
+        // Write your code here
+        
+        if (type.equals("Integer")  || type.equals("Float")){
+            return 4;
+        }else if (type.equals("Long") || type.equals("Double")){
+            return 8;
+        }else {
+            return 1;
+        }
+    }
+    
+}
