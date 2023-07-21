400a383e84774288b6f8fb3363ba258ff0cd2a19
OMAR MAHDI RAFI <97361722+Omar-Mahdi-Rafi@users.noreply.github.com>
2023-07-22 02:47:50 +0600

    Create Pattern9.java

---
+++ Java Basics (Refreshers)/Building Logical Thinking/Pattern9.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    void printDiamond(int n) {
+        // Your code here
+        //Pyramid
+        for(int i = 1; i<=n; i++){
+            for(int j = i; j<=(n-1); j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j <= (2*i)-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //Inverted Pyramid
+        for(int i=n; i>0; i--){
+            //space
+            for(int j=i; j<=n-1; j++){
+                System.out.print(" ");
+            }
+            //asterisk 
+            for(int j=0; j<(2*i-1); j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
